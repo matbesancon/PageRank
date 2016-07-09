159be87225b2f6a44a11d9a09d984101f548a2d3
Mathieu Besancon <mathieu@equisense.fr>
2016-07-10 00:22:43 +0200

    changed output

--- src/main/scala/MainRank.scala
+++ src/main/scala/MainRank.scala
@@ -2,21 +2,24 @@
   * Created by mbesancon on 7/9/16.
   */
 import GraphReader._
-import PageRank._;
+import PageRank._
+import java.io
+import java.io.PrintWriter
 
 import collection.parallel.ParSeq;
 
 object MainRank {
   def main(args: Array[String]) {
-    val graph = if (args.isEmpty) readEdges("/Email-Enron.txt") else readEdges("/" + args.head)
+    val graph = readEdges("/Email-Enron.txt")
     val parGraph: ParSeq[(Int,Int)] = graph.par
     // get all nodes
     val nodeId: ParSeq[Int] = parGraph.map{_._1}.toSet.union(parGraph.map{_._2}.toSet).toSeq
     val startRank: ParSeq[Float] = nodeId.map{x=>1.0f/nodeId.size}
-    val dampArg: Option[Int] = toInt(args(1))
-    val damp = dampArg.getOrElse(0.8f)
+    val damp = 0.8f
     val endRank = compRank(startRank,graph,0.4f,0.001f)
-    nodeId.zip(endRank).foreach(tup=>println(tup._1 + " " + tup._2))
+    // nodeId.zip(endRank).foreach(tup=>println(tup._1 + " " + tup._2))
+    lazy val result: String = nodeId.zip(endRank).map(tup=>(tup._1 + " " + tup._2)).mkString("\n")
+    new PrintWriter("resultRank") { write(result); close}
   }
 
   def toInt(s: String): Option[Int] = {
